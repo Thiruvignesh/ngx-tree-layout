35b813cb54bfeebd45d84dcfad5e93899771369f
Thiru <mastiffthiru@gmail.com>
2022-03-13 06:00:27 +0530

    tree visual rc1 renamed project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {PBICircleComponent} from "./pbicircle/pbicircle.component";
-import {NgxGraphModule} from "@swimlane/ngx-graph";
 import {TreeViewComponent} from './tree-view/tree-view.component';
 
-// import { TreeBubblesComponent } from './tree-bubbles/tree-bubbles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PBICircleComponent,
     TreeViewComponent,
-    // TreeBubblesComponent
   ],
   imports: [
     BrowserModule,
-    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
